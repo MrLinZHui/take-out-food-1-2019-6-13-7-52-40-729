6ae5ffd48d3813fe769533ed15562dad0ddebbb3
MrLinZHui <742847492@qq.com>
2019-07-10 23:21:31 +0800

    finished all function

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,30 +1,85 @@
 var promotions = loadPromotions();
 var items =loadAllItems();
+var PromotionsItem = promotions[1].items;
+//actual time:15min
 function bestCharge(selectedItems) {
-  var ItemsObj =  countOrderSimple(selectedItems);
-  return /*TODO*/;
+  let bestChargeStr = "";
+  let ItemsObj =  countOrderSimple(selectedItems);
+  bestChargeStr = countPayandPrintCharge(ItemsObj);
+  console.log(bestChargeStr);
+  return bestChargeStr;
 }
-//countOrderSimple(["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"])
+//countOrderSimple(["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"]) 
 //actual time:5min
 function countOrderSimple(selectedItems){
-var ItemsObj = [];
-ItemsObj = selectedItems.reduce((itemObj,order) =>{
+return selectedItems.reduce((itemObj,order) =>{
   order = order.replace(" ","");
   let object = order.split("x");
   itemObj[object[0]] = object[1];
   return itemObj;
-},[]);
+},{});
 return ItemsObj;
 }
+//actual time:20min
 function countPayandPrintCharge(ItemsObj){
-  let bestChargeStr = "";
+  let bestChargeStr =`============= 订餐明细 =============\n${ printOrderSimple(ItemsObj)}-----------------------------------\n`;
+  let payArray = PayOrderAll(ItemsObj);
+  let payAll = payArray[0];
+  let payPromotions = payArray[2];
+  let payCompare = payArray[1];
+  if(payAll == payPromotions && payAll == payCompare){
+    bestChargeStr =bestChargeStr+  `总计：${payPromotions}元
+===================================`
+  }
+  else if(payCompare>payPromotions){
+    bestChargeStr =bestChargeStr+ `使用优惠:\n指定菜品半价(黄焖鸡，凉皮)，省${payAll-payPromotions}元
+-----------------------------------
+总计：${payPromotions}元
+===================================`;
+  }
+  else if(payCompare<=payPromotions){
+    bestChargeStr =bestChargeStr+  `使用优惠:\n满30减6元，省6元
+-----------------------------------
+总计：${payCompare}元
+===================================`;
+  }
   return bestChargeStr;
 }
-function PayOrderoffer(ItemsObj){
-  let payOrder = 0.00;
-  return payOrder;
+//actual time:10min
+function PayOrderAll(ItemsObj){
+  let payArray = [];
+  let payAll = 0;
+  let payPromotions = 0;
+  let payCompare = 0;
+  for(let key in ItemsObj){
+    for(let i = 0;i< items.length;i++){
+      if(key === items[i].id){
+        payAll += items[i].price * ItemsObj[key];
+        if(PromotionsItem.indexOf(key)!=-1){
+          payPromotions += (items[i].price * ItemsObj[key])/2;
+        }else{
+          payPromotions += items[i].price * ItemsObj[key];
+        }
+      }
+    }
+  }
+  payCompare = ((payAll >= 30 )? payAll - 6:payAll);
+  payArray.push(payAll);
+  payArray.push(payCompare);
+  payArray.push(payPromotions);
+  //console.log("payPromotions:"+payPromotions);
+  return payArray;
 }
-function countOrderSimple(payOrder){
-  let bestChargeStr = "";
-  return bestChargeStr;
+//actual time:5min
+function printOrderSimple(ItemsObj){
+  let bestChargeStr1 = "";
+  for(let key in ItemsObj){
+    for(let i = 0;i< items.length;i++){
+      if(key === items[i].id){
+        bestChargeStr1 +=`${items[i].name} x${ItemsObj[key]} = ${items[i].price*ItemsObj[key]}元\n`
+      }
+    }
+  }
+  //console.log(bestChargeStr);
+  return bestChargeStr1;
 }
