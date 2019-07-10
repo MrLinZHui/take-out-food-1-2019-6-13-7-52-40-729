4f0a335f9791262dfd33cd3aba023c9ff22f3657
MrLinZHui <742847492@qq.com>
2019-07-10 20:42:43 +0800

    finish function countOrderSimple

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,30 @@
+var promotions = loadPromotions();
+var items =loadAllItems();
 function bestCharge(selectedItems) {
+  var ItemsObj =  countOrderSimple(selectedItems);
   return /*TODO*/;
+}
+//countOrderSimple(["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"])
+//actual time:5min
+function countOrderSimple(selectedItems){
+var ItemsObj = [];
+ItemsObj = selectedItems.reduce((itemObj,order) =>{
+  order = order.replace(" ","");
+  let object = order.split("x");
+  itemObj[object[0]] = object[1];
+  return itemObj;
+},[]);
+return ItemsObj;
+}
+function countPayandPrintCharge(ItemsObj){
+  let bestChargeStr = "";
+  return bestChargeStr;
+}
+function PayOrderoffer(ItemsObj){
+  let payOrder = 0.00;
+  return payOrder;
+}
+function countOrderSimple(payOrder){
+  let bestChargeStr = "";
+  return bestChargeStr;
 }
